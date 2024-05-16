d00a28041dcb292a9d5fab0994cf0a27466f5a55
RahavAyalon <rahav.ayalon@outlook.com>
2024-05-16 07:53:42 +0300

    fix: color scheme

--- client/src/components/History.tsx
+++ client/src/components/History.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-import { RootState } from '../core/store'; // Update the path as needed to correctly import RootState
+import { RootState } from '../core/store';
 
 interface TripMessage {
     request: string;
@@ -10,23 +10,29 @@ interface TripMessage {
 
 function History() {
     const trips = useSelector((state: RootState) => state.data.history as TripMessage[]);
+    const tableStyle = {
+        background: '#212121',
+        borderRadius: '10px',
+        color: 'white',
+        border: '1px solid white'
+    };
 
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="simple table">
+        <TableContainer component={Paper} sx={{ ...tableStyle, minHeight: '50vh', m: 2 }}>
+            <Table aria-label="simple table" sx={tableStyle}>
                 <TableHead>
                     <TableRow>
-                        <TableCell><strong>Request</strong></TableCell>
-                        <TableCell align="right"><strong>Plan</strong></TableCell>
+                        <TableCell sx={tableStyle}><strong>Request</strong></TableCell>
+                        <TableCell align="left" sx={tableStyle}><strong>Plan</strong></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {trips.map((trip, index) => (
                         <TableRow key={index}>
-                            <TableCell component="th" scope="row">
+                            <TableCell component="th" scope="row" sx={tableStyle}>
                                 {trip.request}
                             </TableCell>
-                            <TableCell align="right">
+                            <TableCell align="left" sx={tableStyle}>
                                 {trip.plan}
                             </TableCell>
                         </TableRow>
