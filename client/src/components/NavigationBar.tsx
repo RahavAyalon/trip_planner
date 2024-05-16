c68b67ea45f2be3c8a068db06fe764ffbf900808
RahavAyalon <rahav.ayalon@outlook.com>
2024-05-16 11:35:41 +0300

    fix: refactor client to be more readable

---
+++ client/src/components/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from "@mui/material";
+import { Link as RouterLink } from 'react-router-dom';
+import { getHistoryAsync } from "../features/chat/chatSlice";
+
+// @ts-ignore
+function NavigationBar({ dispatch }) {
+    return (
+        <nav>
+            <Button component={RouterLink} to="/newtrip" style={{ marginRight: 20, color: 'white', border: '1px solid #cfcfcf' }}>
+                Chat
+            </Button>
+            <Button component={RouterLink} to="/recenttrips" style={{ color: 'white', border: '1px solid #cfcfcf' }}
+                onClick={() => dispatch(getHistoryAsync())}
+            >
+                Recent Trips
+            </Button>
+        </nav>
+    );
+}
+
+export default NavigationBar;
