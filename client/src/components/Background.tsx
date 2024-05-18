46957b05b47a484f6f48276bfdf6e83b9ed69578
RahavAyalon <rahav.ayalon@outlook.com>
2024-05-18 13:22:39 +0300

    fetaure: homepage, imporved readability, imports

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -11,7 +11,7 @@ function Background({ dispatch }) {
     };
     return (
         <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }}>
-            <video src="/background.mp4" autoPlay={true} muted style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+            <video src="/logo.mp4" autoPlay={true} muted style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
                 <NavigationBar dispatch={dispatch} style={rootNavStyle} />
             </div>
