68ae60871e493bf6750f447d5cd9947419f0a5ba
RahavAyalon <rahav.ayalon@outlook.com>
2024-05-18 16:03:22 +0300

    chore: try, catch, readability

--- api/src/controllers/trips.py
+++ api/src/controllers/trips.py
@@ -3,7 +3,7 @@ from ..utils.history import update_history
 from ..utils.validator import validate_prompt_input, validate_prompt_injection
 from ..utils.context import context
 from ..utils.tools import tools
-from src.utils.logger import logger
+from ..utils.logger import logger
 
 from dotenv import load_dotenv
 import openai
@@ -14,7 +14,41 @@ load_dotenv()
 
 client = openai.OpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
+    max_retries=3,
+    timeout=10
 )
+
+
+def send_query_to_ai(messages, openai_tools=None, tool_choice=None):
+    try:
+        return client.chat.completions.create(
+            model=os.getenv("OPENAI_MODEL"),
+            messages=messages,
+            tools=openai_tools,
+            tool_choice=tool_choice,
+        )
+    except Exception as e:
+        logger.error("OpenAI API call failed: {}".format(str(e)))
+        return None
+
+
+def add_structured_query_to_messages(tool_call, messages):
+    function_args = json.loads(tool_call.function.arguments)
+    if validate_prompt_input(location=function_args.get("location"),
+                             budget=function_args.get("budget"),
+                             duration=function_args.get("duration")):
+        function_response = json.dumps({
+            "location": function_args.get("location"),
+            "budget": function_args.get("budget"),
+            "duration": function_args.get("duration"),
+            "concept": function_args.get("concept")
+        })
+        messages.append({
+            "role": "function",
+            "tool_call_id": tool_call.id,
+            "name": tool_call.function.name,
+            "content": function_response
+        })
 
 
 def plan_trip_controller(prompt):
@@ -23,49 +57,29 @@ def plan_trip_controller(prompt):
         logger.warning("Prompt injection attempt detected for prompt: " + prompt)
         return {"error": "Prompt injection attempt detected", "status_code": 403}
 
+    cached_response = get_cached_response(prompt)
+    if cached_response:
+        return json.loads(cached_response)
+
     messages = [
         {"role": "system", "content": context},
         {"role": "user", "content": prompt}
     ]
-
-    response = client.chat.completions.create(
-        model=os.getenv("OPENAI_MODEL"),
-        messages=messages,
-        tools=tools,
-        tool_choice="auto",
-    )
+    response = send_query_to_ai(messages=messages, openai_tools=tools, tool_choice="auto")
+    if not response:
+        return {"error": "OpenAI Service Unreachable", "status_code": 503}
     logger.info(str(response.choices))
-    cache_key = prompt
-    cached_response = get_cached_response(cache_key)
-    if cached_response:
-        return json.loads(cached_response)
 
     tool_calls = response.choices[0].message.tool_calls
-    if tool_calls:
-        for tool_call in tool_calls:
-            function_args = json.loads(tool_call.function.arguments)
-            if validate_prompt_input(location=function_args.get("location"),
-                                     budget=function_args.get("budget"),
-                                     duration=function_args.get("duration")):
-                function_response = json.dumps({
-                    "location": function_args.get("location"),
-                    "budget": function_args.get("budget"),
-                    "duration": function_args.get("duration"),
-                    "concept": function_args.get("concept")
-                })
-                messages.append({
-                    "role": "function",
-                    "tool_call_id": tool_call.id,
-                    "name": tool_call.function.name,
-                    "content": function_response
-                })
+    if not tool_calls:
+        return {"content": response.choices[0].message.content}  # Trip information is missing
 
-        enriched_response = client.chat.completions.create(
-            model=os.getenv("OPENAI_MODEL"),
-            messages=messages,
-        )
-        logger.info(str(enriched_response))
-        set_response_in_cache(cache_key, json.dumps(enriched_response.choices[0].message.model_dump()))
-        update_history(prompt, enriched_response)
-        return enriched_response.choices[0].message.model_dump()
-    return {"content": response.choices[0].message.content} # lacking info
+    add_structured_query_to_messages(tool_calls[0], messages)
+    enriched_response = send_query_to_ai(messages=messages)
+    if not response:
+        return {"error": "OpenAI Service Unreachable", "status_code": 503}
+
+    logger.info(str(enriched_response))
+    set_response_in_cache(prompt, json.dumps(enriched_response.choices[0].message.model_dump()))
+    update_history(prompt, enriched_response)
+    return enriched_response.choices[0].message.model_dump()
--- api/src/utils/cache.py
+++ api/src/utils/cache.py
@@ -1,16 +1,23 @@
 import os
 import redis
+from logger import logger
 
 redis_client = redis.StrictRedis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"),
                                  password=os.getenv("REDIS_PASSWORD"), decode_responses=True)
 
 
 def get_cached_response(key):
-    if redis_client.exists(key):
-        return redis_client.get(key)
-    return None
+    try:
+        if redis_client.exists(key):
+            return redis_client.get(key)
+    except Exception as e:
+        logger.warning("Redis error:", e)
 
 
 def set_response_in_cache(key, value, expire_time=3600):
-    redis_client.setex(key, expire_time, value)
+    try:
+        redis_client.setex(key, expire_time, value)
+    except Exception as e:
+        logger.warning("Redis error:", e)
+
 
